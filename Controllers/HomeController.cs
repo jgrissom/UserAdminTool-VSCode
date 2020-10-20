582c498c60d7ed56fad8a1b463b4307576ea9a0d
jgrissom <jgrissom91@gmail.com>
2020-10-20 17:50:59 -0500

    Controller method responds to post requests - Home/Create

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UserAdminTool.Models;
+using System.Threading.Tasks;
 
 namespace UserAdminTool.Controllers
 {
@@ -15,5 +16,31 @@ namespace UserAdminTool.Controllers
 
         public ViewResult Index() => View(userManager.Users);
         public ViewResult Create() => View();
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    UserName = model.Name,
+                    Email = model.Email
+                };
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
     }
 }
