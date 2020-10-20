63bb6f28ee6c4fd06891f6f189225acf45e5e5d3
jgrissom <jgrissom91@gmail.com>
2020-10-20 17:57:12 -0500

    Complete the Edit feature of the user admin tool - Create Edit Method responding to http get - Create Edit Method responding to http post - Create View

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,11 +7,17 @@ namespace UserAdminTool.Controllers
 {
     public class HomeController : Controller
     {
+        private IUserValidator<AppUser> userValidator;
+        private IPasswordValidator<AppUser> passwordValidator;
+        private IPasswordHasher<AppUser> passwordHasher;
         private UserManager<AppUser> userManager;
 
-        public HomeController(UserManager<AppUser> usrMgr)
+        public HomeController(UserManager<AppUser> usrMgr, IUserValidator<AppUser> userValid, IPasswordValidator<AppUser> passValid, IPasswordHasher<AppUser> passwordHash)
         {
             userManager = usrMgr;
+            userValidator = userValid;
+            passwordValidator = passValid;
+            passwordHasher = passwordHash;
         }
 
         public ViewResult Index() => View(userManager.Users);
@@ -42,6 +48,7 @@ namespace UserAdminTool.Controllers
             }
             return View(model);
         }
+
         [HttpPost]
         public async Task<IActionResult> Delete(string id)
         {
@@ -64,6 +71,65 @@ namespace UserAdminTool.Controllers
             }
             return View("Index", userManager.Users);
         }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(string id, string email, string password)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+            if (user != null)
+            {
+                user.Email = email;
+                IdentityResult validEmail = await userValidator.ValidateAsync(userManager, user);
+                if (!validEmail.Succeeded)
+                {
+                    AddErrorsFromResult(validEmail);
+                }
+                IdentityResult validPass = null;
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPass = await passwordValidator.ValidateAsync(userManager, user, password);
+                    if (validPass.Succeeded)
+                    {
+                        user.PasswordHash = passwordHasher.HashPassword(user, password);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPass);
+                    }
+                }
+                if ((validEmail.Succeeded && validPass == null) || (validEmail.Succeeded && password != string.Empty && validPass.Succeeded))
+                {
+                    IdentityResult result = await userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(result);
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View(user);
+        }
+
         private void AddErrorsFromResult(IdentityResult result)
         {
             foreach (IdentityError error in result.Errors)
